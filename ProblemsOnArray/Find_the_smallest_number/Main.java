e5c78b45ef1f43b5fcbd8bb2ac283f94e2bfd0cb
rajnandinitikkal <rajnandinitikkal@gmail.com>
2025-01-15 11:34:39 +0530

    Find the smallest number in an array

---
+++ ProblemsOnArray/Find_the_smallest_number/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args) {
+
+        int arr1[] = {2,5,1,3,0};
+        System.out.println("The smallest element in the array is:" +sort(arr1));
+        
+        int arr2[] = {8,10,5,7,9};
+        System.out.println("The smallest element in the array is:" +sort(arr2));
+               
+        }
+        
+            private static int sort(int[] arr) {
+                Arrays.sort(arr);
+                return arr[0];
+                
+}
+}
---
+++ ProblemsOnArray/Find_the_smallest_number/Readme.md
@@ -0,0 +1,18 @@
+### Problem Statement: 
+Given an array, we have to find the smallest element in the array.
+
+Example 1:
+Input: arr[] = {2,5,1,3,0};
+Output: 0
+Explanation: 0 is the smallest element in the array. 
+
+Example2: 
+Input: arr[] = {8,10,5,7,9};
+Output: 5
+Explanation: 5 is the smallest element in the array.
+
+## [Solution:](Main.java)
+
+We can sort the array in ascending order, hence the smallest element will be at the 0th index. 
+Sort the array in ascending order. Using "sort".
+Print the 0th index.
