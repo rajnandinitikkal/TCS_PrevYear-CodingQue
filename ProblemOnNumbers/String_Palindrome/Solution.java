78f0e07ed13b8668ad45e2681576f3a6e6697663
rajnandinitikkal <rajnandinitikkal@gmail.com>
2025-02-24 14:44:37 +0530

    add string palindrome program

--- ProblemOnNumbers/String_Palindrome/Solution.java
+++ ProblemOnNumbers/String_Palindrome/Solution.java
@@ -8,11 +8,10 @@ public class Solution {
         
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
-        A.charAt(50);
-        /* Enter your code here. Print output to STDOUT. */
+        
         boolean isPalindrome = true;
     
-        for(int i=0; i<=A.length(); i++){
+        for(int i=0; i<A.length()/2; i++){
            if(A.charAt(i) != A.charAt(A.length()-1 -i)){
             isPalindrome = false;
             break;
