d14924d9b9b5de5bdf8e0b140636b1ee0860e45c
rajnandinitikkal <rajnandinitikkal@gmail.com>
2025-02-24 13:29:39 +0530

    add new problem

--- ProblemsOnArray/Find_the_largest_number/Solution.java
+++ ProblemsOnArray/Find_the_largest_number/Solution.java
@@ -18,37 +18,64 @@
 //     }
 // }
 
-import java.io.*;
+// import java.io.*;
+// import java.util.*;
+
+// public class Solution {
+
+//     public static void main(String[] args) {
+        
+//         Scanner sc=new Scanner(System.in);
+//         String A=sc.next();
+//         String B=sc.next();
+//         /* Enter your code here. Print output to STDOUT. */
+        
+//         int A1 = A.length();
+//         int B1 = B.length();
+
+//         int sum = A1 + B1;
+//         System.out.println(sum);
+
+//         if(A1>B1){
+//             System.out.println("Yes");
+//         }
+//         else{
+//             System.out.println("No");
+//         }
+        
+//         String capitalizedA = A.substring(0,1).toUpperCase() + A.substring(1);
+//         String capitalizedB = B.substring(0,1).toUpperCase() + B.substring(1);
+
+//         System.out.println(capitalizedA + " " + capitalizedB);
+        
+//         sc.close();
+        
+//     }
+// }
+
 import java.util.*;
-
-public class Solution {
-
+public class Solution{
     public static void main(String[] args) {
-        
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        String B=sc.next();
-        /* Enter your code here. Print output to STDOUT. */
-        
-        int A1 = A.length();
-        int B1 = B.length();
 
-        int sum = A1 + B1;
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+         
+        int sum = A.length() + B.length();
         System.out.println(sum);
 
-        if(A1>B1){
+        if(A.compareTo(B)> 0){
             System.out.println("Yes");
-        }
-        else{
+        }else{
             System.out.println("No");
         }
-        
-        String capitalizedA = A.substring(0,1).toUpperCase() + A.substring(1);
-        String capitalizedB = B.substring(0,1).toUpperCase() + B.substring(1);
 
-        System.out.println(capitalizedA + " " + capitalizedB);
-        
+        String capatalizedA = A.substring(0,1).toUpperCase()+ A.substring(1);
+        String capatalizedB = B.substring(0,1).toUpperCase()+ B.substring(1);
+
+        System.out.println(capatalizedA +" "+ capatalizedB);
+
         sc.close();
-        
+  
     }
 }
